1b262eb9eeccc12056a8ea7d19c92d3bb5175085
Igor Maznitsa <rrg4400@gmail.com>
2016-09-11 14:49:21 +0300

    updated based SDK to 1.7.1

--- mvn-golang-examples/mvn-golang-example-goexample/src/golang-book/chapter11/math/math_test.go
+++ mvn-golang-examples/mvn-golang-example-goexample/src/golang-book/chapter11/math/math_test.go
@@ -3,9 +3,9 @@ package math
 import "testing"
 
 func TestAverage(t *testing.T) {
-  var v float64
-  v = Average([]float64{1,2})
-  if v != 1.5 {
-    t.Error("Expected 1.5, got ", v)
-  }
+	var v float64
+	v = Average([]float64{1, 2})
+	if v != 1.5 {
+		t.Error("Expected 1.5, got ", v)
+	}
 }
