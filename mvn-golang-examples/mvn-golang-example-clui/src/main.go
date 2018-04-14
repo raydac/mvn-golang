88da46fa5bff9f2629b414c81ac09aeb3d75c18c
Igor Maznitsa <rrg4400@gmail.com>
2018-04-14 12:57:49 +0300

    changed to another demo witout 3th part library

--- mvn-golang-examples/mvn-golang-example-clui/src/main.go
+++ mvn-golang-examples/mvn-golang-example-clui/src/main.go
@@ -1,132 +1,65 @@
 package main
 
 import (
-	"fmt"
 	ui "github.com/VladimirMarkelov/clui"
-	term "github.com/nsf/termbox-go"
-	"strconv"
 )
 
-func updateProgress(value string, pb *ui.ProgressBar) {
-	v, _ := strconv.Atoi(value)
-	pb.SetValue(v)
-}
-
-func changeTheme(lb *ui.ListBox, btn *ui.Button, tp int) {
-	items := ui.ThemeNames()
-	dlgType := ui.SelectDialogRadio
-	if tp == 1 {
-		dlgType = ui.SelectDialogList
-	}
-
-	curr := -1
-	for i, tName := range items {
-		if tName == ui.CurrentTheme() {
-			curr = i
-			break
-		}
-	}
-
-	selDlg := ui.CreateSelectDialog("Choose a theme", items, curr, dlgType)
-	selDlg.OnClose(func() {
-		switch selDlg.Result() {
-		case ui.DialogButton1:
-			idx := selDlg.Value()
-			lb.AddItem(fmt.Sprintf("Selected item: %v", selDlg.Value()))
-			lb.SelectItem(lb.ItemCount() - 1)
-			if idx != -1 {
-				ui.SetCurrentTheme(items[idx])
-			}
-		}
-
-		btn.SetEnabled(true)
-		// ask the composer to repaint all windows
-		ui.PutEvent(ui.Event{Type: ui.EventRedraw})
-	})
-}
-
-func createView() {
-
-	view := ui.AddWindow(0, 0, 20, 7, "Theme Manager Demo")
-
-	frmLeft := ui.CreateFrame(view, 8, 4, ui.BorderNone, 1)
-	frmLeft.SetPack(ui.Vertical)
-	frmLeft.SetGaps(ui.KeepValue, 1)
-	frmLeft.SetPaddings(1, 1)
-
-	frmTheme := ui.CreateFrame(frmLeft, 8, 1, ui.BorderNone, ui.Fixed)
-	frmTheme.SetGaps(1, ui.KeepValue)
-	checkBox := ui.CreateCheckBox(frmTheme, ui.AutoSize, "Use ListBox", ui.Fixed)
-	btnTheme := ui.CreateButton(frmTheme, ui.AutoSize, 4, "Select theme", ui.Fixed)
-	ui.CreateFrame(frmLeft, 1, 1, ui.BorderNone, 1)
-
-	frmPb := ui.CreateFrame(frmLeft, 8, 1, ui.BorderNone, ui.Fixed)
-	ui.CreateLabel(frmPb, 1, 1, "[", ui.Fixed)
-	pb := ui.CreateProgressBar(frmPb, 20, 1, 1)
-	pb.SetLimits(0, 10)
-	pb.SetTitle("{{value}} of {{max}}")
-	ui.CreateLabel(frmPb, 1, 1, "]", ui.Fixed)
-
-	edit := ui.CreateEditField(frmLeft, 5, "0", ui.Fixed)
-
-	frmEdit := ui.CreateFrame(frmLeft, 8, 1, ui.BorderNone, ui.Fixed)
-	frmEdit.SetPaddings(1, 1)
-	frmEdit.SetGaps(1, ui.KeepValue)
-	btnSet := ui.CreateButton(frmEdit, ui.AutoSize, 4, "Set", ui.Fixed)
-	btnStep := ui.CreateButton(frmEdit, ui.AutoSize, 4, "Step", ui.Fixed)
-	ui.CreateFrame(frmEdit, 1, 1, ui.BorderNone, 1)
-	btnQuit := ui.CreateButton(frmEdit, ui.AutoSize, 4, "Quit", ui.Fixed)
-
-	logBox := ui.CreateListBox(view, 28, 5, ui.Fixed)
-
-	ui.ActivateControl(view, edit)
-
-	edit.OnKeyPress(func(key term.Key) bool {
-		if key == term.KeyCtrlM {
-			v := edit.Title()
-			logBox.AddItem(fmt.Sprintf("New PB value(KeyPress): %v", v))
-			logBox.SelectItem(logBox.ItemCount() - 1)
-			updateProgress(v, pb)
-			return true
-		}
-		return false
-	})
-	btnTheme.OnClick(func(ev ui.Event) {
-		btnTheme.SetEnabled(false)
-		tp := checkBox.State()
-		changeTheme(logBox, btnTheme, tp)
-	})
-	btnSet.OnClick(func(ev ui.Event) {
-		v := edit.Title()
-		logBox.AddItem(fmt.Sprintf("New ProgressBar value: %v", v))
-		logBox.SelectItem(logBox.ItemCount() - 1)
-		updateProgress(v, pb)
-	})
-	btnStep.OnClick(func(ev ui.Event) {
-		go pb.Step()
-		logBox.AddItem("ProgressBar step")
-		logBox.SelectItem(logBox.ItemCount() - 1)
-		ui.PutEvent(ui.Event{Type: ui.EventRedraw})
-	})
-	btnQuit.OnClick(func(ev ui.Event) {
-		go ui.Stop()
-	})
-}
-
-func mainLoop() {
-	// Every application must create a single Composer and
-	// call its intialize method
+func main() {
 	ui.InitLibrary()
 	defer ui.DeinitLibrary()
 
-	ui.SetThemePath("themes")
+	view := ui.AddWindow(0, 0, 10, 7, "Hello World!")
+	view.SetPack(ui.Vertical)
 
-	createView()
+	frmResize := ui.CreateFrame(view, 8, 6, ui.BorderNone, ui.Fixed)
+	frmResize.SetTitle("FrameTop")
+	frmResize.SetPack(ui.Horizontal)
+	btn1 := ui.CreateButton(frmResize, 8, 5, "Button 1", 1)
+	btn2 := ui.CreateButton(frmResize, 8, 5, "Button 2", 1)
+	btn3 := ui.CreateButton(frmResize, 8, 5, "Button 3", 1)
 
-	// start event processing loop - the main core of the library
+	frmBtns := ui.CreateFrame(view, 8, 5, ui.BorderNone, ui.Fixed)
+	frmBtns.SetPack(ui.Horizontal)
+	frmBtns.SetTitle("FrameBottom")
+
+	btnHide1 := ui.CreateButton(frmBtns, 8, 4, "Hide 1", 1)
+	btnHide1.OnClick(func(ev ui.Event) {
+		if btn1.Visible() {
+			btnHide1.SetTitle("Show 1")
+			ui.ActivateControl(view, btn1)
+			btn1.SetVisible(false)
+		} else {
+			btnHide1.SetTitle("Hide 1")
+			btn1.SetVisible(true)
+		}
+	})
+	btnHide2 := ui.CreateButton(frmBtns, 8, 4, "Hide 2", 1)
+	btnHide2.OnClick(func(ev ui.Event) {
+		if btn2.Visible() {
+			btnHide2.SetTitle("Show 2")
+			ui.ActivateControl(view, btn2)
+			btn2.SetVisible(false)
+		} else {
+			btnHide2.SetTitle("Hide 2")
+			btn2.SetVisible(true)
+		}
+	})
+	btnHide3 := ui.CreateButton(frmBtns, 8, 4, "Hide 3", 1)
+	btnHide3.OnClick(func(ev ui.Event) {
+		if btn3.Visible() {
+			btnHide3.SetTitle("Show 3")
+			ui.ActivateControl(view, btn3)
+			btn3.SetVisible(false)
+		} else {
+			btnHide3.SetTitle("Hide 3")
+			btn3.SetVisible(true)
+		}
+	})
+
+	btnQuit := ui.CreateButton(frmBtns, 8, 4, "Quit", 1)
+	btnQuit.OnClick(func(ev ui.Event) {
+		go ui.Stop()
+	})
+
 	ui.MainLoop()
-}
-
-func main() {
-	mainLoop()
 }
