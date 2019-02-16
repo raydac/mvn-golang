1d78118497a543cee7eef2c1eefa9a2e0351c98a
Igor Maznitsa <rrg4400@gmail.com>
2019-02-16 20:23:48 +0200

    improved test

--- mvn-golang-examples/mvn-golang-example-maven-repository/mvn-go-test-app/src/main.go
+++ mvn-golang-examples/mvn-golang-example-maven-repository/mvn-go-test-app/src/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "com/igormaznitsa/mvngotest"
+import (
+	"com/igormaznitsa/mvngotest"
+	"com/igormaznitsa/mvngotesttwo"
+)
 
 func main() {
 	mvngotest.SomeTestMethod()
+	mvngotesttwo.SomeTestMethodTwo()
 }
---
+++ mvn-golang-examples/mvn-golang-example-maven-repository/mvn-go-test-libtwo/src/com/igormaznitsa/mvngotesttwo/SomeUtils.go
@@ -0,0 +1,13 @@
+package mvngotesttwo
+
+import (
+	meta "com/igormaznitsa/mvngotestmeta"
+	"fmt"
+)
+
+var Buildstamp string
+var svnRevision string
+
+func SomeTestMethodTwo() {
+	fmt.Printf(meta.SomeMetaMethod("mvn-go-test-libtwo module") + "!\n")
+}
