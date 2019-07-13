3cf0c64b046ad3ff41e3db6a5de742ced95001a6
Igor Maznitsa <rrg4400@gmail.com>
2019-07-13 21:40:03 +0300

    refactoring

--- mvn-golang-wrapper/src/main/java/com/igormaznitsa/mvngolang/AbstractGoDependencyAwareMojo.java
+++ mvn-golang-wrapper/src/main/java/com/igormaznitsa/mvngolang/AbstractGoDependencyAwareMojo.java
@@ -307,7 +307,7 @@ public abstract class AbstractGoDependencyAwareMojo extends AbstractGolangMojo {
             this.getLog().debug("Restoring go.mod from backup in source folder: " + srcFolder);
             this.restoreGoModFromBackupAndRemoveBackup(srcFolder);
           } else {
-            this.getLog().info("Restoring go.mod from backup is disabled");
+            this.getLog().warn("Restoring of go.mod from backup is disabled");
           }
         }
       }
