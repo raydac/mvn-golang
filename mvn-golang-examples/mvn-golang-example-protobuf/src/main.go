2db221d1e7ae7e1f5e4532481217203849f13f80
Igor Maznitsa <rrg4400@gmail.com>
2019-08-05 22:16:22 +0300

    added example for protobuf

---
+++ mvn-golang-examples/mvn-golang-example-protobuf/src/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"log"
+	"protoexample"
+)
+
+func main() {
+
+	elliot := &protoexample.Person{
+		Name: "Elliot",
+		Age:  24,
+	}
+
+	data, err := proto.Marshal(elliot)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+
+	// printing out our raw protobuf object
+	fmt.Println(data)
+
+	// let's go the other way and unmarshal
+	// our byte array into an object we can modify
+	// and use
+	newElliot := &protoexample.Person{}
+	err = proto.Unmarshal(data, newElliot)
+	if err != nil {
+		log.Fatal("unmarshaling error: ", err)
+	}
+
+	// print out our `newElliot` object
+	// for good measure
+	fmt.Println(newElliot.GetAge())
+	fmt.Println(newElliot.GetName())
+
+}
