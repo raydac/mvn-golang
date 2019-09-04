adae597c9f816b4e479269c64c3d4521bd9460b4
Igor Maznitsa <rrg4400@gmail.com>
2019-09-04 09:25:27 +0300

    default golang sdk version updated to 1.13

--- mvn-golang-wrapper/src/main/java/com/igormaznitsa/mvngolang/AbstractGolangMojo.java
+++ mvn-golang-wrapper/src/main/java/com/igormaznitsa/mvngolang/AbstractGolangMojo.java
@@ -353,7 +353,7 @@ public abstract class AbstractGolangMojo extends AbstractMojo {
    * The Go SDK version. It plays role if goRoot is undefined. Can be defined
    * through system property 'mvn.golang.go.version'
    */
-  @Parameter(name = "goVersion", defaultValue = "1.12.9", property = "mvn.golang.go.version")
+  @Parameter(name = "goVersion", defaultValue = "1.13", property = "mvn.golang.go.version")
   private String goVersion;
 
   /**
