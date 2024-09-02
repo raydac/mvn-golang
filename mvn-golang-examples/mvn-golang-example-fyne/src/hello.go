717bfac2bd37c60373f7afdd77da42aa730b246b
Igor Maznitsa <rrg4400@gmail.com>
2024-09-02 10:58:14 +0300

    added example for Fyne

---
+++ mvn-golang-examples/mvn-golang-example-fyne/src/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
+}
